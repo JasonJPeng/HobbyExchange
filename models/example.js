70b26080b455c1f4cc04f3069f0bede14c632203
Jason Peng <jason.j.peng@gamil.com>
2019-07-29 14:17:14 -0700

    server.js

--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Example = sequelize.define("User", {
+    name: DataTypes.STRING,
+    aboiutMe: DataTypes.TEXT
   });
   return Example;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,9 +9,9 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/user", function(req, res) {
+    db.User.create(req.body).then(function(result) {
+      res.json(result);
     });
   });
 
