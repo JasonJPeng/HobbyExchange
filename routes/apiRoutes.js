aadceab07696d4c88641629227a60a7f9b55d893
Jason Peng <jason.j.peng@gamil.com>
2019-08-01 13:17:48 -0700

    work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,105 @@
 var db = require("../models");
 
-async function teachToUsers(id) {
-   let allTeach = await db.Teach.findAll({
-                           where:{user_id: id}
-                        });
+async function findHobby(id) {
+  return new Promise((resolve, reject) => {
+    db.Hobby.findAll({
+      where: { id: id }
+    }).then(function (data) {
+      resolve(data[0].name);
+    })
+  })
+}
 
-   var myTeach = allTeach.map(e=>{return e.hobby_id});
-   let teachTo = await db.Learn.findAll({
-                          where: {hobby_id: myTeach}
-                        })                                      
-    return teachTo;
+async function findUser(id) {
+  return new Promise((resolve, reject) => {
+    db.User.findAll({
+      where: {id:id}
+    }).then(function(data){
+      resolve(data[0]);
+    })
+  })
+}
+
+async function getMatchData(elementT, elementL) {
+  console.log('getMatchData');
+  return new Promise(async (resolve, reject) => {
+    let teach_subject = await findHobby(elementT.hobby_id);
+    let learn_subject = await findHobby(elementL.hobby_id);
+    let user_obj = await findUser(elementT.user_id);
+    let obj = {
+      user_id: elementT.user_id,
+      user_name: user_obj.name,
+      user_about: user_obj.about_me,
+      user_photo: user_obj.photo_url,
+      user_email: user_obj.email,
+      teach_id: elementT.hobby_id,
+      teach_subject: teach_subject,
+      teach_info: elementT.Description,
+      learn_id: elementL.hobby_id,
+      learn_subject: learn_subject,
+      learn_info: elementL.Description
+    };
+    resolve(obj)
+  })
+
+}
+
+async function findMatch(teachTo, learnFrom) {
+
+  return new Promise(async (resolve, reject) => {
+    let temp = [];
+    for (var i = 0; i < teachTo.length; i++) {
+      for (var j = 0; j < learnFrom.length; j++) {
+        if (teachTo[i].user_id === learnFrom[j].user_id) {
+          temp.push(getMatchData(teachTo[i], learnFrom[j]));
+        }
+      }
+      // if(i === teachTo.length -1) {
+      //   Promise.all(temp).then(data => {
+      //     // console.log(data);
+      //     resolve(data);
+            
+      //   });    
+      // }
+    }
+    Promise.all(temp).then(data => {
+      resolve(data);
+    })
+  });// end of promise
+}
+
+
+
+
+async function teachToUsers(id) {
+  let allTeach = await db.Teach.findAll({
+    where: { user_id: id }
+  });
+
+  var myTeach = allTeach.map(e => { return e.hobby_id });
+  let teachTo = await db.Learn.findAll({
+    where: { hobby_id: myTeach }
+  })
+
+  return teachTo;
 }
 
 async function learnFromUsers(id) {
   let allLearn = await db.Learn.findAll({
-                          where:{user_id: id}
-                       });
+    where: { user_id: id }
+  });
 
-  var myLearn = allLearn.map(e=>{return e.hobby_id});
+  var myLearn = allLearn.map(e => { return e.hobby_id });
   let learnFrom = await db.Teach.findAll({
-                         where: {hobby_id: myLearn}
-                       })
-   return learnFrom;
+    where: { hobby_id: myLearn }
+  })
+  return learnFrom;
 }
 
-async function createUser (req) {
+async function createUser(req) {
   var output = {};
 
-// Create a new user  
+  // Create a new user  
   let userData = await db.User.create({
     name: req.body.name,
     email: req.body.email,
@@ -35,85 +107,86 @@ async function createUser (req) {
   });
   output.user = userData;
   var userId = userData.id;
-// create multiple teaching skills
+  // create multiple teaching skills
   let teachData = await db.Teach.bulkCreate(
-      [
-        {user_id: userId,
-         hobby_id: req.body.hobby_id1,
-         Description: req.body.desc_1 
-        },
-        {user_id: userId,
+    [
+      {
+        user_id: userId,
+        hobby_id: req.body.hobby_id1,
+        Description: req.body.desc_1
+      },
+      {
+        user_id: userId,
         hobby_id: req.body.hobby_id2,
-        Description: req.body.desc_2 
-        },
-        {user_id: userId,
-         hobby_id: req.body.hobby_id3,
-         Description: req.body.desc_3 
-        }
-      ]
+        Description: req.body.desc_2
+      },
+      {
+        user_id: userId,
+        hobby_id: req.body.hobby_id3,
+        Description: req.body.desc_3
+      }
+    ]
   );
   output.teach = teachData;
 
-// create multiple learning needs 
+  // create multiple learning needs 
   let learnData = await db.Learn.bulkCreate(
     [
-      {user_id: userId,
-       hobby_id: req.body.learn_id1,
-       Description: req.body.learn_desc_1 
+      {
+        user_id: userId,
+        hobby_id: req.body.learn_id1,
+        Description: req.body.learn_desc_1
       },
-      {user_id: userId,
-      hobby_id: req.body.learn_id2,
-      Description: req.body.learn_desc_2 
+      {
+        user_id: userId,
+        hobby_id: req.body.learn_id2,
+        Description: req.body.learn_desc_2
       },
-      {user_id: userId,
-       hobby_id: req.body.learn_id3,
-       Description: req.body.learn_desc_3 
+      {
+        user_id: userId,
+        hobby_id: req.body.learn_id3,
+        Description: req.body.learn_desc_3
       }
     ]
-);
-output.learn = learnData;
+  );
+  output.learn = learnData;
 
-// find who need to teach ==== teachTo
-teachTo = await teachToUsers(userId); 
-for (var i=0; i< teachTo.length; i++) {
-  var obj = teachTo[i];
-  let data = await db.User.findOne({ where: {id: obj.user_id}})
-  obj.username = data.name;
-}    
-output.teachTo = teachTo;
+  // find who need to teach ==== teachTo
+  teachTo = await teachToUsers(userId);
 
-// find who can I learn from ==== learnFrom
-output.learnFrom = await learnFromUsers(userId); 
-// output.learnFrom = "FFFFFFFF";
+  output.teachTo = teachTo;
 
-output['message'] = "CCCCCC"
-output.teachTo[1]['message'] = "OOOOOOOO"
+  // find who can I learn from ==== learnFrom
+  output.learnFrom = await learnFromUsers(userId);
 
-return output;
-  
+  output.match = await findMatch(output.teachTo, output.learnFrom);
+
+
+  return output;
+
 }
 
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/hobbies", function(req, res) {
-    db.Hobby.findAll({}).then(function(dbExamples) {
+  app.get("/api/hobbies", function (req, res) {
+    db.Hobby.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
-  app.get("/api/users/:userid/skills", function(req, res) {
+  app.get("/api/users/:userid/skills", function (req, res) {
     db.Learn.findAll({
-      where:{
+      where: {
         user_id: req.params.userid
       }
-    }).then(function(data) {
-      var myLearn = data.map(e=>{return e.hobby_id})
+    }).then(function (data) {
+      var myLearn = data.map(e => { return e.hobby_id })
       db.Teach.findAll({
         where: {
           hobby_id: myLearn
         }
-      }).then(function (result){
+      }).then(function (result) {
 
         res.json(result);
       })
@@ -121,19 +194,19 @@ module.exports = function(app) {
       // res.json(data);
     });
   });
-//  
-  app.get("/api/users/:userid/teachto", function(req, res) {
+  //  
+  app.get("/api/users/:userid/teachto", function (req, res) {
     db.Teach.findAll({
-      where:{
+      where: {
         user_id: req.params.userid
       }
-    }).then(function(data) {
-      var myTeach = data.map(e=>{return e.hobby_id})
+    }).then(function (data) {
+      var myTeach = data.map(e => { return e.hobby_id })
       db.Learn.findAll({
         where: {
           hobby_id: myTeach
         }
-      }).then(function (result){
+      }).then(function (result) {
 
         res.json(result);
       })
@@ -144,143 +217,68 @@ module.exports = function(app) {
 
 
   // Create a new example
-  app.post("/api/users", async function (req,res) {
-     let data = await createUser(req); 
-     res.json(data);
+  app.post("/api/users", async function (req, res) {
+    let data = await createUser(req);
+    res.json(data);
   })
 
-///////////////////////////////////////////////
-//       var resultAll = {
-//         user: result
-//       };
-//       var userId = result.id;
-//       var arrObj = [
-//         {user_id: userId,
-//          hobby_id: req.body.hobby_id1,
-//          Description: req.body.desc_1 
-//         },
-//         {user_id: userId,
-//           hobby_id: req.body.hobby_id2,
-//           Description: req.body.desc_2 
-//          },
-//          {user_id: userId,
-//           hobby_id: req.body.hobby_id3,
-//           Description: req.body.desc_3 
-//          }
-//       ];
-//       var arrObjLearn = [
-//         {user_id: userId,
-//          hobby_id: req.body.learn_id1,
-//          Description: req.body.learn_desc_1 
-//         },
-//         {user_id: userId,
-//           hobby_id: req.body.learn_id2,
-//           Description: req.body.learn_desc_2 
-//          },
-//          {user_id: userId,
-//           hobby_id: req.body.learn_id3,
-//           Description: req.body.learn_desc_3 
-//          }
-//       ]; 
-//     })    
+  // /api/users/:userid/skills for teach.html
+  app.post("/api/users/:userid/skills", function (req, res) {
+    var arrObj = [
+      {
+        user_id: req.params.userid,
+        hobby_id: req.body.hobby_id1,
+        Description: req.body.desc_1
+      },
+      {
+        user_id: req.params.userid,
+        hobby_id: req.body.hobby_id2,
+        Description: req.body.desc_2
+      },
+      {
+        user_id: req.params.userid,
+        hobby_id: req.body.hobby_id3,
+        Description: req.body.desc_3
+      }
+    ];
+    db.Teach.bulkCreate(arrObj).then(function (result) {
+      res.json(result);
+    });
 
-// // Add records to learns table and teaches table
-//       db.Teach.bulkCreate(arrObj).then(function(resultTeach) {
-//        resultAll.teach = resultTeach;
-//        db.Learn.bulkCreate(arrObjLearn).then(function(resultLearn) {
-//         resultAll.learn = resultLearn
-//         // res.json(resultAll);
-//        });
-// // ///////////////////////////////////////////////////
-// // Create a match jspn
-// //
-//      var match = {teachto: {}, learnfrom: {}};
-    
-//      db.Teach.findAll({
-//       where:{
-//         user_id: userId
-//       }
-//     }).then(function(data) {
-//       var myTeach = data.map(e=>{return e.hobby_id})
-//       db.Learn.findAll({
-//         where: {
-//           hobby_id: myTeach
-//         }
-//       }).then(function (resultTeachTo){
-//         match.teachto = resultTeachTo;
 
-//         db.Learn.findAll({
-//           where:{
-//             user_id: userId
-//           }
-//         }).then(function(data) {
-//           var myLearn = data.map(e=>{return e.hobby_id});
-//           db.Teach.findAll({
-//             where: {
-//               hobby_id: myLearn
-//             }
-//           }).then(function (resultLearnFrom){
-//             match.learnfrom = resultLearnFrom;
-//             res.json(match);
-//           })  // end of db.Teach
-//       }) ; // end of db.Learn
-
-//       // res.json(data); 
-//       });  
-//     });
-//   });
-//////////////////////////////////////////////////////////
- // /api/users/:userid/skills for teach.html
- app.post("/api/users/:userid/skills", function(req, res) {
-  var arrObj = [
-    {user_id: req.params.userid,
-     hobby_id: req.body.hobby_id1,
-     Description: req.body.desc_1 
-    },
-    {user_id: req.params.userid,
-      hobby_id: req.body.hobby_id2,
-      Description: req.body.desc_2 
-     },
-     {user_id: req.params.userid,
-      hobby_id: req.body.hobby_id3,
-      Description: req.body.desc_3 
-     }
-  ];
-  db.Teach.bulkCreate(arrObj).then(function(result) {
-   res.json(result);
   });
 
 
-});
+  // /api/users/:userid/needs triggered by learn.html
+  app.post("/api/users/:userid/needs", function (req, res) {
+    var arrObj = [
+      {
+        user_id: req.params.userid,
+        hobby_id: req.body.hobby_id1,
+        Description: req.body.desc_1
+      },
+      {
+        user_id: req.params.userid,
+        hobby_id: req.body.hobby_id2,
+        Description: req.body.desc_2
+      },
+      {
+        user_id: req.params.userid,
+        hobby_id: req.body.hobby_id3,
+        Description: req.body.desc_3
+      }
+    ];
+    db.Learn.bulkCreate(arrObj).then(function (result) {
+      res.json(result);
+    });
 
-
- // /api/users/:userid/needs triggered by learn.html
- app.post("/api/users/:userid/needs", function(req, res) {
-  var arrObj = [
-    {user_id: req.params.userid,
-     hobby_id: req.body.hobby_id1,
-     Description: req.body.desc_1 
-    },
-    {user_id: req.params.userid,
-      hobby_id: req.body.hobby_id2,
-      Description: req.body.desc_2 
-     },
-     {user_id: req.params.userid,
-      hobby_id: req.body.hobby_id3,
-      Description: req.body.desc_3 
-     }
-  ];
-  db.Learn.bulkCreate(arrObj).then(function(result) {
-   res.json(result);
   });
-
-});
 
 
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
