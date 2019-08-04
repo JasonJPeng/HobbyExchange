c4979dbbc3065a401689dc3c095a3f4c67982e6f
Jason Peng <jason.j.peng@gamil.com>
2019-08-04 16:03:35 -0700

    add root

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.sendFile(index.html)
+    res.sendFile(path.join(__dirname , "..",  "public", "index.html"));
     // db.Example.findAll({}).then(function(dbExamples) {
     //   res.render("index", {
     //     msg: "Welcome!",
